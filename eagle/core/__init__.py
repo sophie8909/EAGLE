7a3a3c7dd71369120066be67fc2811ca8056f89a
sophie8909 <34826797+sophie8909@users.noreply.github.com>
2026-05-29 14:36:00 +0800

    refactor(core): add task plugin interface

--- eagle/core/__init__.py
+++ eagle/core/__init__.py
@@ -2,6 +2,7 @@
 
 from .algorithm import BaseAlgorithm
 from .individual import Individual
+from .plugin import BaseTaskPlugin, ObjectiveValues, ParsedOutput, TaskPlugin
 from .registry import (
     ALGORITHMS,
     CROSSOVER_OPERATORS,
@@ -17,14 +18,18 @@ from .result import EvaluationResult, ensure_evaluation_result
 __all__ = [
     "ALGORITHMS",
     "BaseAlgorithm",
+    "BaseTaskPlugin",
     "CROSSOVER_OPERATORS",
     "ENVIRONMENTAL_SELECTION",
     "EvaluationResult",
     "EVALUATORS",
     "Individual",
     "MUTATION_OPERATORS",
+    "ObjectiveValues",
     "PARENT_SELECTION",
+    "ParsedOutput",
     "REFLECTION_OPERATORS",
     "Registry",
+    "TaskPlugin",
     "ensure_evaluation_result",
 ]
